16df59e7dfcafaa3047355fe9aa5b512737a19c3
SethFSchool <144287186+SethFSchool@users.noreply.github.com>
2023-10-10 08:18:54 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-from flask import Flask
 from markupsafe import escape
-app = Flask(__name__)
+
 @app.route("/<name>")
 def hello(name):
-  return f'Hello, {escape(name)}!'
+    return f"Hello, {escape(name)}!"
