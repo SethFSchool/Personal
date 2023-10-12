23fdc88916975ce456de9e715f659f5f33e6436c
SethFSchool <144287186+SethFSchool@users.noreply.github.com>
2023-10-12 08:45:27 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,26 +1,27 @@
 from tkinter import *
-from tkinter import messagebox
+import os
+import pickle
 
 scr=Tk()
-scr.geometry("500x500")
-scr.title("Login")
+scr.geometry("400x400")
+scr.title("Logger")
 
-label=Label(scr,text="Login",font=("",40))
+label=Label(scr,text="Entry Logger",font=("",40))
 label.pack(anchor="center")
 
-lb2=Label(scr,text="Email:",font=("",30))
-lb2.place(x=10,y-90)
+lb2=Label(scr,text="Name:",font=("",30))
+lb2.place(x=10,y=90)
 
 in1=Entry(scr)
-in1.place(x=200,y=100)
+in1.place(x=150,y=107)
 
-lb3=Label(window,text="Password:",font=("",30))
+lb3=Label(scr,text="Email:",font=("",30))
 lb3.place(x=10,y=170)
 
 in2=Entry(scr)
-in2.place(x=200,y=100)
+in2.place(x=150,y=187)
 
-bn=Button(scr,text="Apply",font=("",25))
-bn.place(x=150,y=300)
+bn=Button(scr,text="Access Robot Controls",font=("",25))
+bn.place(x=0,y=300)
 
 scr.mainloop()
